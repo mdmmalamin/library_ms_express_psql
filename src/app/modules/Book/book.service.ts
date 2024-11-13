65ce18c556287846b5de4207711c3eb411541bd9
mdmmalamin <mdmmalamin@gmail.com>
2024-11-14 05:13:04 +0600

    add getAllBooks & getBookById  done

--- src/app/modules/Book/book.service.ts
+++ src/app/modules/Book/book.service.ts
@@ -1,27 +1,40 @@
+import { Book } from "@prisma/client";
 import { prisma } from "../../../shared";
 import { TBook } from "./book.interface";
 
-const createBookIntoDB = (payload: TBook) => {
-  const result = prisma.book.create({
+const createBookIntoDB = async (payload: TBook): Promise<Book> => {
+  const result = await prisma.book.create({
     data: payload,
   });
 
   return result;
 };
 
-const getAllBooksFromDB = () => {
-  console.log("getAllBooksFromDB");
+const getAllBooksFromDB = async () => {
+  const result = await prisma.book.findMany();
+
+  return result;
 };
 
-const getBookByIdFromDB = (bookId: string) => {
-  console.log(bookId);
+const getBookByIdFromDB = async (bookId: string): Promise<Book | null> => {
+  const result = await prisma.book.findUnique({
+    where: {
+      bookId,
+    },
+  });
+
+  return result;
 };
 
-const updateBookByIdIntoDB = (bookId: string) => {
-  console.log(bookId);
+const updateBookByIdIntoDB = async (
+  bookId: string,
+  payload: Partial<Book>
+): Promise<Book> => {
+  console.log([bookId, payload]);
+  return payload;
 };
 
-const deleteBookByIdFromDB = (bookId: string) => {
+const deleteBookByIdFromDB = async (bookId: string) => {
   console.log(bookId);
 };
 
